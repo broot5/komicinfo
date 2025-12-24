83792da3fec0c8a44d2bdc0f87f7fbd066dff218
broot5 <29516943+broot5@users.noreply.github.com>
2025-12-25 05:29:44 +0900

    fix: fallback when atomic move unsupported

--- src/jvmAndAndroidMain/kotlin/io/github/broot5/komicinfo/internal/AtomicReplace.kt
+++ src/jvmAndAndroidMain/kotlin/io/github/broot5/komicinfo/internal/AtomicReplace.kt
@@ -1,16 +1,26 @@
 package io.github.broot5.komicinfo.internal
 
 import java.io.File
+import java.nio.file.AtomicMoveNotSupportedException
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 
 internal object AtomicReplace {
   fun moveTempIntoPlace(tempFile: File, destination: File) {
-    Files.move(
-        tempFile.toPath(),
-        destination.toPath(),
-        StandardCopyOption.REPLACE_EXISTING,
-        StandardCopyOption.ATOMIC_MOVE,
-    )
+    try {
+      Files.move(
+          tempFile.toPath(),
+          destination.toPath(),
+          StandardCopyOption.REPLACE_EXISTING,
+          StandardCopyOption.ATOMIC_MOVE,
+      )
+    } catch (_: AtomicMoveNotSupportedException) {
+      // Fall back when atomic moves aren't supported
+      Files.move(
+          tempFile.toPath(),
+          destination.toPath(),
+          StandardCopyOption.REPLACE_EXISTING,
+      )
+    }
   }
 }
